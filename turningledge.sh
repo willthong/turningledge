bc2c1ce1e00fc909ef8ca5fe7410b3655532bf67
Will Thong <willthong@gmail.com>
2017-02-08 20:54:44 +0000

    First commit

---
+++ turningledge.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+# turningledge.sh
+
+_V=0
+while getopts "v" OPTION
+do
+    case $OPTION in
+        v) _V=1
+            ;;
+    esac
+done
+
+function log () {
+    if  [[ $_V -eq 1 ]]; then
+        printf "$@"
+    fi
+}
+
+
+printf "************************\n"
+printf "* Turning Ledge Report *\n"
+printf "*      %s      *\n"         "$(date +"%d/%m/%Y")"
+printf "************************\n"
+
+log "Reading config file...\n"
+source ~/.turningledgerc
+
+log "\n"
+log "Income \n"
+log "====== \n"
+
+function monthlyincome {
+    ledger budget $salaryaccount \
+        -p "last 30 days" \
+        --budget-format "%(get_at(display_total, 1))\n"
+}
+monthlyincome=$(tr -dc '0-9.' <<< $(monthlyincome))
+
+function monthlyincomeex {
+    ledger budget \
+    $regularexpensesinclude \
+    -p "last 30 days" \
+    --budget-format "%(trim(scrub(get_at(T, 1))))\n" -n | awk "END {print $NF}"
+}
+monthlyincomeex=$(tr -dc '0-9.' <<< $(monthlyincomeex))
+
+log "Deriving salary from account \"%s\".\n" "$salaryaccount"
+log "Adding \"%s\" to monthly expense calculations.\n\n" "$regularexpensesinclude" 
+log "Monthly income:             £ %7s \n" "$monthlyincome"
+log "    Less regular expenses:  £ %7s \n" "$monthlyincomeex"
+
+log "\n"
+log "Unbudgeted expenses\n"
+log "===================\n"
+
+function unbudgetedday {
+    ledger --unbudgeted register not "Credit Card" \
+        -p "last day" \
+        --format "%(display_total)\n" --tail 1
+}
+unbudgetedday=$(tr -dc '0-9.' <<< $(unbudgetedday))
+if [[ -z $(unbudgetedday) ]] ; then
+    unbudgetedday=0.00
+else
+    unbudgetedday=$(tr -dc '0-9.' <<< $(unbudgetedday))
+fi
+
+function unbudgetedweek {
+    ledger --unbudgeted register not "Credit Card" \
+        -p "last week" \
+        --format "%(display_total)\n" --tail 1
+}
+if [[ -z $(unbudgetedweek) ]] ; then
+    unbudgetedweek=0.00
+else
+    unbudgetedweek=$(tr -dc '0-9.' <<< $(unbudgetedweek))
+fi
+
+function unbudgetedmonth {
+    ledger --unbudgeted register not "Credit Card" \
+        -p "last 30 days" \
+        --format "Past month: %(display_total)\n" --tail 1
+}
+if [[ -z $(unbudgetedmonth) ]] ; then
+    unbudgetedmonth=0.00
+else
+    unbudgetedmonth=$(tr -dc '0-9.' <<< $(unbudgetedmonth))
+fi
+
+function unbudgetedyear {
+    ledger --unbudgeted register not "Credit Card" \
+        -p "last year" \
+        --format "Past year: %(display_total)\n" --tail 1
+}
+if [[ -z $(unbudgetedyear) ]] ; then
+    unbudgetedyear=0.00
+else
+    unbudgetedyear=$(tr -dc '0-9.' <<< $(unbudgetedyear))
+fi
+
+log "Past day:      £ %7s\n" "$unbudgetedday"
+log "Past week:     £ %7s\n" "$unbudgetedweek"
+log "Past month:    £ %7s\n" "$unbudgetedmonth"
+log "Past year:     £ %7s\n" "$unbudgetedyear"
+
+printf "\n"
+printf "Allowable spending\n"
+printf "==================\n"
+
+function allowableday {
+    echo "scale=2; ($monthlyincomeex/30.4375)-$unbudgetedday" | bc
+}
+
+function allowableweek {
+    echo "scale=2; ($monthlyincomeex/4.348)-$unbudgetedweek" | bc
+}
+
+function allowablemonth {
+    echo "scale=2; $monthlyincomeex-$unbudgetedmonth" | bc
+}
+
+function allowableyear {
+    echo "scale=2; ($monthlyincomeex*12)-$unbudgetedyear" | bc
+}
+
+printf "Today:         £ %7s\n" "$(allowableday)"
+printf "This week:     £ %7s\n" "$(allowableweek)"
+printf "This month:    £ %7s\n" "$(allowablemonth)"
+printf "This year:     £ %7s\n" "$(allowableyear)"
